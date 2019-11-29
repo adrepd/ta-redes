5cc900bec5ab4d53a70a8d537cc4d2893834b910
Adrian <aeulloa@pucp.edu.pe>
2019-11-28 22:20:33 -0500

    first commit

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'database-ta-redes.cjwfotr20cvp.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'admin123',
+        database: 'db_links'
+    }
+
+};
